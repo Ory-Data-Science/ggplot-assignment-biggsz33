595b4ecfb908a023135bc61d2fa6fefe69b1c6d7
Biggs <Zachary.Biggs@stlcop.local>
2017-10-31 20:34:27 -0500

    Number 3 Part 1

--- Number3.R
+++ Number3.R
@@ -6,3 +6,6 @@ dat <- read.csv(url("http://esapubs.org/archive/ecol/E084/093/Mammal_lifehistori
                 sep = "\t", nrows = 1440, na.strings = c("-999.00", "999.00"))
 
 #rest of your code goes here
+ggplot(dat, aes(x = mass.g., y = newborn.g.)) +
+  geom_point(size = 3, color = "black") +
+  labs(x = "Adult Mass (grams)", y = ("Newborn Mass (grams)"))
