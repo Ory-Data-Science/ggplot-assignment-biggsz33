6f47a730c631dad918d15eb39d9bd66e1c5ba771
Biggs <Zachary.Biggs@stlcop.local>
2017-10-31 20:39:24 -0500

    Added Part Two

--- Number3.R
+++ Number3.R
@@ -9,3 +9,7 @@ dat <- read.csv(url("http://esapubs.org/archive/ecol/E084/093/Mammal_lifehistori
 ggplot(dat, aes(x = mass.g., y = newborn.g.)) +
   geom_point(size = 3, color = "black") +
   labs(x = "Adult Mass (grams)", y = ("Newborn Mass (grams)"))
+
+ggplot(dat, aes(x = log10(mass.g.), y = log10(newborn.g.))) + 
+  geom_point(size =2, color =  "blue") +
+  labs( x = "Log Adult Mass", y = "Log Newborn Mass")
